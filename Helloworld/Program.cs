a834c6d14e2529f61ca1aa7f8f8aeaffa318a511
Sten Sinkarev <sten.sinkarev@gmail.com>
2022-02-04 11:32:56 +0200

    Create Helloworld Program

---
+++ Helloworld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string Username;
+            Username = Console.ReadLine();
+            Console.WriteLine("Hello," + Username);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
